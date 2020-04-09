d946dbceb31f8245a1f5a5cc1f6701be372538f9
ligatorDaniel <danielulloa256@gmail.com>
2020-04-09 03:40:01 -0400

    arreglamos nombre footer x header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlayerComponent } from './components/player/player.component';
 import { CarrouselComponent } from './components/carrousel/carrousel.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
@@ -14,7 +14,7 @@ import { environment } from '../environments/environment';
     AppComponent,
     PlayerComponent,
     CarrouselComponent,
-    FooterComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
